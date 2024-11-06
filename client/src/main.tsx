a6fdc42572be85570482af0c1f2f0268ee3c7440
Adharika11 <adhs.gs2003@gmail.com>
2024-11-06 12:08:37 +0530

    Provider Setup and Basket Slice

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,8 +7,14 @@ import {createRoot} from "react-dom/client";
 import {StrictMode} from "react";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./app/router/Routes.tsx";
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore.tsx';
+
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router = {router}/>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
     </StrictMode>,
 )
